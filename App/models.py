411dfdf970939f87ac0d689db729c5b38695ba60
Shaharia-SE <shahariarony38@gmail.com>
2022-01-13 21:43:37 +0600

    PortFolio update

--- App/models.py
+++ App/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class about(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200, null=True)
+
+
+    def __str__(self):
+        return self.name
--- App/views.py
+++ App/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import about
+
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    aboutdata = about.objects.all()[0]
+    context = {
+        'about': aboutdata
+    }
+    return render(request, 'index.html', context)
+
