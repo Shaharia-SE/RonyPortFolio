6a0ea4aacfd161e23e87bdf985aa0643fc284ac6
Shaharia-SE <shahariarony38@gmail.com>
2022-01-14 22:56:27 +0600

    PortFolio update

--- App/models.py
+++ App/models.py
@@ -4,8 +4,19 @@ from django.db import models
 # Create your models here.
 class about(models.Model):
     name = models.CharField(max_length=200, null=True)
-    title = models.CharField(max_length=200, null=True)
-    bio = models.CharField(max_length=2000, null=True)
-
+    title = models.CharField(max_length=200, blank=True)
+    bio = models.CharField(max_length=2000, blank=True)
+    firstname = models.CharField(max_length=200, blank=True)
+    fathername = models.CharField(max_length=200, blank=True)
+    mothername = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=200, blank=True)
+    mail = models.CharField(max_length=200, blank=True)
+    Address = models.CharField(max_length=200, blank=True)
     def __str__(self):
         return self.name
+
+
+
+
+
+
